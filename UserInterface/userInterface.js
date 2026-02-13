15e9a6f4207efeb283c9e32578ecfda4dc9468e3
Mafalda Moura <mafaldamoura@MacBook-Air-de-Mafalda.local>
2026-02-13 16:09:01 +0000

    atualização aula 13/02 - criação das funções | organização info app.js

--- UserInterface/userInterface.js
+++ UserInterface/userInterface.js
@@ -18,12 +18,43 @@ REFLEXÃO:
 DESAFIO:
 Como aplicar classes diferentes para receita e despesa?
 */
+import { transactions } from "../transactions/transactions.js";
+import { getTotalDespesas, getTotalReceitas, getSaldo } from "../calculations/calculations.js";
 
-let transacao = document.querySelector(".lista-transacao");
+const listaTransacao = document.querySelector(".lista-transacoes");
 
-function renderTransactions (){
-   const listaTransacao = document.querySelector(".lista-transacao");
-   listaTransacao.innerHTML = ""; //isto "esvazia" a caixa antes de voltar a preencher a mesma 
 
-   listaTransacao.f
+export function renderTransactions() { //renderizar para aparecer na tela. É esta função que é responsável por isto. 
+   let novaTransacao = ''; //acumular uma string no HTML. Esta variável é a mesma que tem de estar no forEach 
+
+   transactions.forEach((t) => { //forEach percorre um array  // span para criar blocos //quando ele é chamado, a informação já existe lá 
+      //span t.tipo vai ser responsável por garantir que se t.tipo for uma receita, vai colocar a etiqueta-receita. Se não mete a etiqueta despesa 
+      // span tvalor vai colocar os valores ou como positivos ou como negativos
+      novaTransacao = novaTransacao +
+        `<div class = "transaction item-transacao" data-id="${t.id}" > 
+            <span> ${t.descricao} </span>
+            <span class="${t.tipo === "receita" ? "etiqueta-receita" : "etiqueta-despesa"}">${t.tipo}</span> 
+            <span> ${t.data} </span>
+            <span>${t.tipo === "receita" ? "+" : "-"} ${Number(t.valor).toLocaleString("pt-PT", { style: "currency", currency: "EUR" })}</span>
+            <button class="btn-remover" title="Remover">✕</button>
+
+        </div>`;
+    })
+
+   listaTransacao.innerHTML= novaTransacao; //meter a lista dentro do meu container 
+}
+
+
+export function atualizacaoCards (lista){ //Recebo a lista, calculo os 3 números e meto-os nos cards 
+   const saldoValor = document.querySelector("#saldoValor");  //vou buscar ao documento do index onde é que estão os cards. 
+   const  receitasValor = document.querySelector("#receitasValor"); 
+   const despesasValor = document.querySelector("#despesasValor"); 
+
+   const saldo = getSaldo(lista); //chamo as funções que vou precisar de trabalhar para fazer que ele pegue nas funções que eu fiz. 
+   const receita = getTotalReceitas(lista); 
+   const despesa = getTotalDespesas(lista);
+
+   saldoValor.textContent = saldo //escrevo no texto/dashboard, o cálculo do  que eu preciso 
+   receitasValor.textContent = receita; 
+   despesasValor.textContent = despesa; 
 }
--- app.js
+++ app.js
@@ -23,21 +23,34 @@ O que deve acontecer quando a página recarrega?
 
 
 const botao = document.querySelector(".adiciona-historia"); 
-import { addTransactions, getTransactions, transactions } from "./transactions/transactions";
+
+import {transactions, carregarTransactions, guardarTransactions, getTransactions, addTransactions} from "./transactions/transactions.js";
+import { renderTransactions, atualizacaoCards } from "./UserInterface/userInterface.js";
+
+carregarTransactions(); 
+renderTransactions();
+atualizacaoCards();
+
 
 botao.addEventListener("click", () => {
-    let descricao = document.querySelector("#descricao").value;
-    let quanTipo = document.querySelector("#quantidade").value;
-    let tipo = document.querySelector("#tipo-transacao").value; 
+    const descricao = document.querySelector("#descricao");
+    const quanTipo = document.querySelector("#quantidade");
+    const tipo = document.querySelector("#tipo-transacao"); 
+
+    const descricaoValor = descricao.value; // a variável tem de guardar o elemento, não o texto. 
+    const quanTipoValor = quanTipo.value; 
+    const tipoValor = tipo.value; 
 
     const valorNumero = Number(quanTipo);
 
     if (descricao.trim() === "" || quanTipo.trim() === "" || tipo.trim() === ""){
-        alert("Por favor, preencha o dado!")
+        alert("Por favor, preencha os dados!")
+        return;
     }
 
     if(Number.isNaN(valorNumero) || valorNumero <= 0){
         alert("Por favor, insira um valor válido!");
+        return; // para ele parar e não continuar. Sem este return ele deixava que eu continuasse a preencher em branco ou com valores não válidos 
     }
 
     const novaTransacao = { // tem de ser criado aqui dentro porque ele depende de coisas em que eu vou clicar. Ou seja, eu preencho e depois crio a nova Transação para ficar guardado 
@@ -48,7 +61,15 @@ botao.addEventListener("click", () => {
         data: new Date().toLocaleDateString("pt-PT"),
     }
 
-    addTransactions(novaTransacao); //aqui estou a guardar o objeto que criei no array. 
-    console.log(getTransactions); 
+    addTransactions(novaTransacao); //aqui estou a guardar o objeto que criei no array.
+    renderTransactions(); // faz a renderização 
+    atualizacaoCards(getTransactions());
+
+    //Apagar os inputs 
+
+    descricao.value=""; //responsáveis por apagar os inputs 
+    quanTipo.value=""; 
+    tipo.value=""; 
 });
+
 
